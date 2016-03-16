df2623f381206c2f87321a3ef7b381d3a8bc9e69
cheng <chengcm2011@163.com>
2016-03-17 07:43:52 +0800

    update logback

--- src/main/java/cheng/msdp/access/web/LoginAction.java
+++ src/main/java/cheng/msdp/access/web/LoginAction.java
@@ -12,6 +12,8 @@ import cheng.lib.util.URLUtil;
 import com.github.diamond.server.Constants;
 import com.github.diamond.server.netty.DiamondServer;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,64 +23,75 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-@Controller public class LoginAction extends BusinessCommonAction {
-    @RequestMapping("/login") public String login(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+@Controller
+public class LoginAction extends BusinessCommonAction {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	IDataBaseService queryservice = ApplicationServiceLocator.getService(IDataBaseService.class);
-	String action = getAction("page", request);
-	if ("page".equals(action)) {
-	    String pk_role = request.getParameter("pk_role");
-	    DiamondServer.getInstance().getServerHandler().pushConfig("cheng", "production", pk_role + Constants.PROFILE_separator);
-	    return "login";
-	} else {
-	    String username = request.getParameter("username");
-	    String password = request.getParameter("password");
-	    if (StringUtils.isBlank(username)) {
-		return "login";
-	    }
-	    if (isroot(username, password)) {
-		ClientThreadData.setClientUser(getRootUser());
-		MsdpUserSessionVO msdpUserSessionVO = new MsdpUserSessionVO(getRootUser());
-		HttpRequestUtil.setUserToSession(request, response, msdpUserSessionVO);
-		String backToUrl = getBackUrl(request);
-		return "redirect:" + backToUrl;
-	    }
-	    if (StringUtils.isBlank(password)) {
-		return "login";
-	    }
 
-	    List<UserModel> listu = queryservice.queryByClause(UserModel.class, " dr=0 and user_code='" + username + "'");
-	    if (listu == null || listu.size() == 0) {
-		//model.addAttribute(Data, list);
-		return "login";
-	    }
-	    UserModel user = listu.get(0);
-	    DesUtil en = new DesUtil();
-	    if (user.getUser_password().equals(en.encode(password))) {
-		ClientThreadData.setClientUser(user);
-		MsdpUserSessionVO msdpUserSessionVO = new MsdpUserSessionVO(user);
-		HttpRequestUtil.setUserToSession(request, response, msdpUserSessionVO);
-		String backToUrl = getBackUrl(request);
-		return "redirect:" + backToUrl;
-	    }
-	    return "login";
-	}
+
+
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+        logger.error("error");
+        logger.info("info");
+        logger.debug("debug");
+
+        IDataBaseService queryservice = ApplicationServiceLocator.getService(IDataBaseService.class);
+        String action = getAction("page", request);
+        if ("page".equals(action)) {
+            String pk_role = request.getParameter("pk_role");
+            DiamondServer.getInstance().getServerHandler().pushConfig("cheng", "production", pk_role + Constants.PROFILE_separator);
+            return "login";
+        } else {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            if (StringUtils.isBlank(username)) {
+                return "login";
+            }
+            if (isroot(username, password)) {
+                ClientThreadData.setClientUser(getRootUser());
+                MsdpUserSessionVO msdpUserSessionVO = new MsdpUserSessionVO(getRootUser());
+                HttpRequestUtil.setUserToSession(request, response, msdpUserSessionVO);
+                String backToUrl = getBackUrl(request);
+                return "redirect:" + backToUrl;
+            }
+            if (StringUtils.isBlank(password)) {
+                return "login";
+            }
+
+            List<UserModel> listu = queryservice.queryByClause(UserModel.class, " dr=0 and user_code='" + username + "'");
+            if (listu == null || listu.size() == 0) {
+                //model.addAttribute(Data, list);
+                return "login";
+            }
+            UserModel user = listu.get(0);
+            DesUtil en = new DesUtil();
+            if (user.getUser_password().equals(en.encode(password))) {
+                ClientThreadData.setClientUser(user);
+                MsdpUserSessionVO msdpUserSessionVO = new MsdpUserSessionVO(user);
+                HttpRequestUtil.setUserToSession(request, response, msdpUserSessionVO);
+                String backToUrl = getBackUrl(request);
+                return "redirect:" + backToUrl;
+            }
+            return "login";
+        }
     }
 
     private boolean isroot(String username, String password) {
-	if (username.equals("root") && password.equals("1qaz2wsx3edc4rfv")) {
-	    return true;
-	}
-	return false;
+        if (username.equals("root") && password.equals("1qaz2wsx3edc4rfv")) {
+            return true;
+        }
+        return false;
     }
 
     private UserModel getRootUser() {
-	UserModel u = new UserModel();
-	u.setPk_role("00000000000000000000");
-	u.setPk_user("00000000000000000000");
-	u.setUser_code("root");
-	u.setUser_name("root");
-	return u;
+        UserModel u = new UserModel();
+        u.setPk_role("00000000000000000000");
+        u.setPk_user("00000000000000000000");
+        u.setUser_code("root");
+        u.setUser_name("root");
+        return u;
     }
 
     /**
@@ -86,25 +99,27 @@ import java.util.List;
      * @return
      */
     private String getBackUrl(HttpServletRequest request) {
-	String backToUrl = request.getParameter("backToUrl");
-	if (backToUrl == null || backToUrl.trim().length() == 0) {
-	    backToUrl = "/management";
-	} else {
-	    backToUrl = URLUtil.encode(backToUrl);
-	}
-	return backToUrl;
+        String backToUrl = request.getParameter("backToUrl");
+        if (backToUrl == null || backToUrl.trim().length() == 0) {
+            backToUrl = "/management";
+        } else {
+            backToUrl = URLUtil.encode(backToUrl);
+        }
+        return backToUrl;
     }
 
-    @RequestMapping("/logout") public String logout(HttpServletRequest request, HttpServletResponse response) {
-	removeUserFromSession(request, response);
-	return "redirect:login";
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        removeUserFromSession(request, response);
+        return "redirect:login";
     }
 
-    @RequestMapping("/register") public String register(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping("/register")
+    public String register(HttpServletRequest request, HttpServletResponse response) {
 
-	removeUserFromSession(request, response);
+        removeUserFromSession(request, response);
 
-	return "redirect:register";
+        return "redirect:register";
     }
 
 }
--- src/main/java/cheng/msdp/ui/trade/BusinessProcessAction.java
+++ src/main/java/cheng/msdp/ui/trade/BusinessProcessAction.java
@@ -28,64 +28,64 @@ import java.util.Map;
 @Controller
 public class BusinessProcessAction extends BusinessCommonAction {
     @RequestMapping("/management/ui/businessprocess/delete")
-    public ModelAndView delete(HttpServletRequest request, DeleteActionParamVO paramvo ,Model model) throws BusinessException {
-	IDataBaseService queryservice = ApplicationServiceLocator.getService(IDataBaseService.class);
+    public ModelAndView delete(HttpServletRequest request, DeleteActionParamVO paramvo, Model model) throws BusinessException {
+        IDataBaseService queryservice = ApplicationServiceLocator.getService(IDataBaseService.class);
 
-	String pk_node = paramvo.getTemplateid();
-	NodeModel node = queryservice.queryByPK(NodeModel.class, pk_node);
-	SuperModel supervo = (SuperModel) ClassUtil.initClass(node.getModelclass());
-	queryservice.deleteByPK(supervo.getClass(), paramvo.getPk_data());
+        String pk_node = paramvo.getTemplateid();
+        NodeModel node = queryservice.queryByPK(NodeModel.class, pk_node);
+        SuperModel supervo = (SuperModel) ClassUtil.initClass(node.getModelclass());
+        queryservice.deleteByPK(supervo.getClass(), paramvo.getPk_data());
 
-	return ajaxDoneSuccess("删除成功");
+        return ajaxDoneSuccess("删除成功");
 
     }
 
 
     @RequestMapping("/management/ui/businessprocess/save")
-    public ModelAndView save(SaveActionParamVO paramvo ,HttpServletRequest request,Model model) throws BusinessException {
-	IDataBaseService queryservice = ApplicationServiceLocator.getService(IDataBaseService.class);
+    public ModelAndView save(SaveActionParamVO paramvo, HttpServletRequest request, Model model) throws BusinessException {
+        IDataBaseService queryservice = ApplicationServiceLocator.getService(IDataBaseService.class);
 
-	String pk_node = paramvo.getTemplateid().split(",")[0];
-	paramvo.setTemplateid(pk_node);
-	NodeModel node = (NodeModel)queryservice.queryByPK(NodeModel.class, pk_node);
-	//接收到节点的pk 解析 模型 反射得到值
-	SuperModel supervo = (SuperModel) ClassUtil.initClass(node.getModelclass());
-	//接收到所有字段和值
-	Map<String, Object> values = getParamFromReq(request);
+        String pk_node = paramvo.getTemplateid().split(",")[0];
+        paramvo.setTemplateid(pk_node);
+        NodeModel node = (NodeModel) queryservice.queryByPK(NodeModel.class, pk_node);
+        //接收到节点的pk 解析 模型 反射得到值
+        SuperModel supervo = (SuperModel) ClassUtil.initClass(node.getModelclass());
+        //接收到所有字段和值
+        Map<String, Object> values = getParamFromReq(request);
 
-	initBean(supervo ,values);
-	//保存之前处理要保存的数据 从按钮中取得classname
-	ButtonModel button = queryservice.queryByPK(ButtonModel.class, paramvo.getPk_button());
-	IButtonAction buttonaction = null ;
-	if(StringUtils.isNotEmpty(button.getActionclass())){
-	    buttonaction = (IButtonAction) ClassUtil.initClass(button.getActionclass());//IButtonAction
-	    supervo = buttonaction.beforeSaveAction(supervo, paramvo, request);
-	}
-	//功能节点 添加下级节点需要
-	if(StringUtils.isNotEmpty(paramvo.getPk_parent())){
-	    if(supervo.getAttributeValue(supervo.getParentPKFieldName())==null || StringUtils.isEmpty(supervo.getAttributeValue(supervo.getParentPKFieldName()).toString())){
-		BeanHelper.setProperty(supervo, supervo.getParentPKFieldName(), paramvo.getPk_parent());
-	    }
-	}
-	if(StringUtils.isEmpty(supervo.getPrimaryKey())){
-	    queryservice.insert(supervo);
-	}else{
-	    queryservice.update(supervo);
-	}
-	if(buttonaction!=null){
-	    buttonaction.afterSaveAction(supervo, paramvo, request);
-	}
-	return ajaxDoneSuccess("保存成功");
+        initBean(supervo, values);
+        //保存之前处理要保存的数据 从按钮中取得classname
+        ButtonModel button = queryservice.queryByPK(ButtonModel.class, paramvo.getPk_button());
+        IButtonAction buttonaction = null;
+        if (StringUtils.isNotEmpty(button.getActionclass())) {
+            buttonaction = (IButtonAction) ClassUtil.initClass(button.getActionclass());//IButtonAction
+            supervo = buttonaction.beforeSaveAction(supervo, paramvo, request);
+        }
+        //功能节点 添加下级节点需要
+        if (StringUtils.isNotEmpty(paramvo.getPk_parent())) {
+            if (supervo.getAttributeValue(supervo.getParentPKFieldName()) == null || StringUtils.isEmpty(supervo.getAttributeValue(supervo.getParentPKFieldName()).toString())) {
+                BeanHelper.setProperty(supervo, supervo.getParentPKFieldName(), paramvo.getPk_parent());
+            }
+        }
+        if (StringUtils.isEmpty(supervo.getPrimaryKey())) {
+            queryservice.insert(supervo);
+        } else {
+            queryservice.update(supervo);
+        }
+        if (buttonaction != null) {
+            buttonaction.afterSaveAction(supervo, paramvo, request);
+        }
+        return ajaxDoneSuccess("保存成功");
 
     }
 
     private SuperModel initBean(SuperModel supervo, Map<String, Object> values) {
-	Iterator<Map.Entry<String, Object>> lt = values.entrySet().iterator();
-	while(lt.hasNext()){
-	    Map.Entry<String, Object> entry = lt.next();
-	    BeanHelper.setProperty(supervo, entry.getKey(), entry.getValue());
-	}
-	return supervo;
+        Iterator<Map.Entry<String, Object>> lt = values.entrySet().iterator();
+        while (lt.hasNext()) {
+            Map.Entry<String, Object> entry = lt.next();
+            BeanHelper.setProperty(supervo, entry.getKey(), entry.getValue());
+        }
+        return supervo;
     }
 
 }
